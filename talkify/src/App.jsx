4e45d1a587a554120940c8a3b081e9fdddb33945
CodeWithShashankJain <87229559+CodeWithShashankJain@users.noreply.github.com>
2022-04-08 22:17:45 +0530

    setup and configured firebase

--- talkify/src/App.jsx
+++ talkify/src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      
+      <h3 className="text-3xl">world</h3>
+      <input type="text"></input>
     </div>
   );
 }

9297cc0b72d6196a96c9ca966a2c32fe9051864e
piyush-sharma-06 <68019524+piyush-sharma-06@users.noreply.github.com>
2022-04-30 23:58:58 +0530

    added loader component (#3)
    Co-authored-by: Shashank Jain <87229559+CodeWithShashankJain@users.noreply.github.com>

--- talkify/src/Components/Loading.jsx
+++ talkify/src/Components/Loading.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {BiLoaderAlt} from "react-icons/bi"
-const Loading = () => {
-  return (
-      <h1 className=" h-screen flex text-4xl justify-center items-center ">
-        <BiLoaderAlt className = "h-16 w-16 animate-spin"/>
-      </h1>
-  );
-};
+ import React from 'react'
+ import {BiLoaderAlt} from "react-icons/bi"
 
-export default Loading;
+ const Loading = () => {
+     return (
+             <h2 className="h-screen flex text-4xl justify-center items-center">
+                 <BiLoaderAlt className="h-16 w-16 animated-spin"/>
+            </h2>
+     );
+ };
+
+ export default Loading;
--- talkify/src/context/Auth.js
+++ talkify/src/context/Auth.js
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 import Loading from "../Components/Loading";
+
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
@@ -14,7 +15,7 @@ const AuthProvider = ({ children }) => {
       setLoading(false);
     });
   }, []);
-  if (loading) return <Loading/>;
+  if (loading) return <Loading />;
   // else {
   //      return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
   // }

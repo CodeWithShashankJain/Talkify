c660a0e5d5db4bf99ad27e3322467775f57eccc9
Anmol Pandey <pandeyanmol079@gmail.com>
2022-05-06 22:13:33 +0530

    used localstorage to persist the state on reloding the page

--- talkify/src/pages/Auth/Login.jsx
+++ talkify/src/pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Button from "../../Components/Button";
 import { Link } from "react-router-dom";
 import { Alert, TextField } from "@mui/material";
@@ -36,6 +36,15 @@ const Login = () => {
         break;
     }
   };
+  useEffect(() => {
+    const forms = JSON.parse(localStorage.getItem("user"));
+    if (email === "" && password === "")
+      setData((prev) => ({ ...prev, ...forms }));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(data));
+  }, [email, password]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -65,67 +74,68 @@ const Login = () => {
   };
   return (
     <div className="flex h-screen items-center justify-center">
-    <div
-      className="w-3/6 mx-auto my-8 p-4 border
+      <div
+        className="w-3/6 mx-auto my-8 p-4 border
     -4 bg-white rounded-lg shadow-2xl "
-    >
-      <h1 className="mt-4 font-serif text-4xl font-semibold text-center">
-        Talkify
-      </h1>
-      <p className="mt-4 text-sm font-semibold text-center">
-        Join the community to chat with your freinds
-      </p>
-      <form
-        component="form"
-        className="mx-6 mt-4 text-center"
-        onSubmit={handleSubmit}
       >
-        <div className="flex flex-col space-y-6 items-center">
-          <TextField
-            label="Email"
-            id="email"
-            variant="outlined"
-            size="small"
-            value={data.email}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            variant="outlined"
-            type="password"
-            size="small"
-            value={data.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {error
-          ? emailErrorVisibility && (
-              <Alert
-                onClose={() => {
-                  setEmailErrorVisibility(false);
-                }}
-                className="absolute left-0 right-0 mx-auto transition duration-1000 ease-in-out w-96 top-10"
-                severity="error"
-              >
-                {emailError}
-              </Alert>
-            )
-          : null}
-        <p className="mt-3 text-sm font-semibold text-center cursor-pointer">
-          New User ?
-          <Link to="/register">
-            <span className="hover:underline"> Register</span>
-          </Link>
+        <h1 className="mt-4 font-serif text-4xl font-semibold text-center">
+          Talkify
+        </h1>
+        <p className="mt-4 text-sm font-semibold text-center">
+          Join the community to chat with your freinds
         </p>
-        <Button className="mt-3 block ml-auto w-32" disabled={loading}>
-          {loading ? "Logging in..." : "Login"}
-        </Button>
-      </form>
+        <form
+          component="form"
+          className="mx-6 mt-4 text-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col space-y-6 items-center">
+            <TextField
+              label="Email"
+              id="email"
+              type="email"
+              variant="outlined"
+              size="small"
+              value={data.email}
+              onChange={handleChange}
+              required
+            />
+            <TextField
+              label="Password"
+              id="password"
+              type="password"
+              variant="outlined"
+              size="small"
+              value={data.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {error
+            ? emailErrorVisibility && (
+                <Alert
+                  onClose={() => {
+                    setEmailErrorVisibility(false);
+                  }}
+                  className="absolute left-0 right-0 mx-auto transition duration-1000 ease-in-out w-96 top-10"
+                  severity="error"
+                >
+                  {emailError}
+                </Alert>
+              )
+            : null}
+          <p className="mt-3 text-sm font-semibold text-center cursor-pointer">
+            New User ?
+            <Link to="/register">
+              <span className="hover:underline"> Register</span>
+            </Link>
+          </p>
+          <Button className="mt-3 block ml-auto w-32" disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </Button>
+        </form>
       </div>
-      </div>
+    </div>
   );
 };
 

f0178294da729aaa1b6ce8426369c3ce1a9f097c
anmol079 <91677309+anmol079@users.noreply.github.com>
2022-05-06 22:49:31 +0530

    used localstorage to persist the state on reloding the page (#4)

--- talkify/src/pages/Auth/Register.jsx
+++ talkify/src/pages/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../../Components/Button";
 import { Link } from "react-router-dom";
 import { Alert, TextField } from "@mui/material";
@@ -38,6 +38,16 @@ const Register = () => {
         break;
     }
   };
+  useEffect(() => {
+    const forms = JSON.parse(localStorage.getItem("user"));
+    if (name === "" && email === "" && password === "")
+      setData((prev) => ({ ...prev, ...forms }));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(data));
+  }, [name, email, password]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setData({ ...data, error: null, loading: true });
@@ -76,74 +86,73 @@ const Register = () => {
 
   return (
     <div className="flex h-screen items-center justify-center">
-    <div
-      className="w-3/6 mx-auto my-8 p-4 border
+      <div
+        className="w-3/6 mx-auto my-8 p-4 border
     -4 bg-white rounded-lg shadow-2xl "
-    >
-      <h1 className="mt-4 font-serif text-4xl font-semibold text-center">
-        Talkify
-      </h1>
-      <p className="mt-4 text-sm font-semibold text-center">
-        Join the community to chat with your freinds
-      </p>
-      <form
-        className="mx-6 mt-4 text-center"
-        onSubmit={handleSubmit}
       >
-        <div className="flex flex-col space-y-6 items-center">
-          <TextField
-            label="Name"
-            id="name"
-            variant="outlined"
-            size="small"
-            value={data.name}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            label="Email"
-            id="email"
-            variant="outlined"
-            size="small"
-            value={data.email}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            variant="outlined"
-            type="password"
-            size="small"
-            value={data.passwordl}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {error
-          ? emailErrorVisibility && (
-              <Alert
-                onClose={() => {
-                  setEmailErrorVisibility(false);
-                }}
-                className="absolute left-0 right-0 mx-auto transition duration-1000 ease-in-out w-96 top-10"
-                severity="error"
-              >
-                {emailError}
-              </Alert>
-            )
-          : null}
-        <p className="mt-3 text-sm font-semibold text-center cursor-pointer">
-          Already have an account?{" "}
-          <Link to="/login">
-            <span className="hover:underline">Login</span>
-          </Link>
+        <h1 className="mt-4 font-serif text-4xl font-semibold text-center">
+          Talkify
+        </h1>
+        <p className="mt-4 text-sm font-semibold text-center">
+          Join the community to chat with your freinds
         </p>
-        <Button className="mt-3 block ml-auto w-28" disabled={loading}>
-          {loading ? "Creating..." : "Register"}
-        </Button>
-      </form>
-      {/* {emailErrorVisibility && (
+        <form className="mx-6 mt-4 text-center" onSubmit={handleSubmit}>
+          <div className="flex flex-col space-y-6 items-center">
+            <TextField
+              label="Name"
+              id="name"
+              type="text"
+              variant="outlined"
+              size="small"
+              value={data.name}
+              onChange={handleChange}
+              required
+            />
+            <TextField
+              label="Email"
+              id="email"
+              type="email"
+              variant="outlined"
+              size="small"
+              value={data.email}
+              onChange={handleChange}
+              required
+            />
+            <TextField
+              label="Password"
+              id="password"
+              variant="outlined"
+              type="password"
+              size="small"
+              value={data.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {error
+            ? emailErrorVisibility && (
+                <Alert
+                  onClose={() => {
+                    setEmailErrorVisibility(false);
+                  }}
+                  className="absolute left-0 right-0 mx-auto transition duration-1000 ease-in-out w-96 top-10"
+                  severity="error"
+                >
+                  {emailError}
+                </Alert>
+              )
+            : null}
+          <p className="mt-3 text-sm font-semibold text-center cursor-pointer">
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className="hover:underline">Login</span>
+            </Link>
+          </p>
+          <Button className="mt-3 block ml-auto w-28" disabled={loading}>
+            {loading ? "Creating..." : "Register"}
+          </Button>
+        </form>
+        {/* {emailErrorVisibility && (
         <Alert
           onClose={() => {
             setEmailErrorVisibility(false);
@@ -155,7 +164,7 @@ const Register = () => {
         </Alert>
       )} */}
       </div>
-      </div>
+    </div>
   );
 };
 
